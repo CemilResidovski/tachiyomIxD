3579b8d6e1df13078c398d0996251bf00a990f69
Cemil Residovski <Cemil.Residovski@gmail.com>
2017-03-01 09:54:02 +0100

    Added initial bottom navigation bar

--- app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.TaskStackBuilder
 import android.support.v4.view.GravityCompat
 import android.view.MenuItem
+import android.view.View
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.ui.backup.BackupFragment
@@ -27,10 +28,10 @@ class MainActivity : BaseActivity() {
 
     private val startScreenId by lazy {
         when (preferences.startScreen()) {
-            1 -> R.id.nav_drawer_library
-            2 -> R.id.nav_drawer_recently_read
+            1 -> R.id.nav_bottom_library
+            2 -> R.id.nav_bottom_recently_read
             3 -> R.id.nav_drawer_recent_updates
-            else -> R.id.nav_drawer_library
+            else -> R.id.nav_bottom_library
         }
     }
 
@@ -44,8 +45,29 @@ class MainActivity : BaseActivity() {
             return
         }
 
+        //bottom_view.findViewById(R.id.unchecked_bottom_item).visibility = View.GONE
+        //nav_view.findViewById(R.id.unchecked_nav_item).visibility = View.GONE
+
         // Inflate activity_main.xml.
         setContentView(R.layout.activity_main)
+
+        // Handle bottom bar
+        bottom_view.setOnNavigationItemSelectedListener { item ->
+            val id = item.itemId
+
+            // Reset selection on nav view
+            nav_view.setCheckedItem(R.id.unchecked_nav_item)
+
+            val oldFragment = supportFragmentManager.findFragmentById(R.id.frame_container)
+            if (oldFragment == null || oldFragment.tag.toInt() != id) {
+                when (id) {
+                    R.id.nav_bottom_library -> setFragment(LibraryFragment.newInstance(), id)
+                    R.id.nav_bottom_recently_read -> setFragment(RecentlyReadFragment.newInstance(), id)
+                    R.id.nav_bottom_catalogues -> setFragment(CatalogueFragment.newInstance(), id)
+                }
+            }
+            true
+        }
 
         // Handle Toolbar
         setupToolbar(toolbar, backNavigation = false)
@@ -56,15 +78,15 @@ class MainActivity : BaseActivity() {
             // Make information view invisible
             empty_view.hide()
 
+            // Check invisible item on bottom bar if item pressed on nav bar
+            bottom_view.menu.findItem(R.id.unchecked_bottom_item).isChecked = true
+
             val id = item.itemId
 
             val oldFragment = supportFragmentManager.findFragmentById(R.id.frame_container)
             if (oldFragment == null || oldFragment.tag.toInt() != id) {
                 when (id) {
-                    R.id.nav_drawer_library -> setFragment(LibraryFragment.newInstance(), id)
                     R.id.nav_drawer_recent_updates -> setFragment(RecentChaptersFragment.newInstance(), id)
-                    R.id.nav_drawer_recently_read -> setFragment(RecentlyReadFragment.newInstance(), id)
-                    R.id.nav_drawer_catalogues -> setFragment(CatalogueFragment.newInstance(), id)
                     R.id.nav_drawer_latest_updates -> setFragment(LatestUpdatesFragment.newInstance(), id)
                     R.id.nav_drawer_downloads -> startActivity(Intent(this, DownloadActivity::class.java))
                     R.id.nav_drawer_settings -> {
@@ -77,10 +99,10 @@ class MainActivity : BaseActivity() {
             drawer.closeDrawer(GravityCompat.START)
             true
         }
-
         if (savedState == null) {
             // Set start screen
-            setSelectedDrawerItem(startScreenId)
+            //setSelectedDrawerItem(startScreenId)
+            setStartScreen()
 
             // Show changelog if needed
             ChangelogDialogFragment.show(this, preferences, supportFragmentManager)
@@ -101,7 +123,8 @@ class MainActivity : BaseActivity() {
             drawer.closeDrawers()
         } else if (fragment != null && fragment.tag.toInt() != startScreenId) {
             if (resumed) {
-                setSelectedDrawerItem(startScreenId)
+                //setSelectedDrawerItem(startScreenId)
+                setStartScreen()
             }
         } else {
             super.onBackPressed()
@@ -124,6 +147,11 @@ class MainActivity : BaseActivity() {
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    // because start screen is not in nav bar
+    private fun setStartScreen() {
+        bottom_view.menu.performIdentifierAction(startScreenId, 0)
     }
 
     private fun setSelectedDrawerItem(itemId: Int, triggerAction: Boolean = true) {
